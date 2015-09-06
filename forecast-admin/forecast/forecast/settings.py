69de590584363a7891343aa82d63382377ed3bb9
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2015-09-05 21:55:04 -0400

    updated for postgres

--- forecast-admin/forecast/forecast/settings.py
+++ forecast-admin/forecast/forecast/settings.py
@@ -78,13 +78,16 @@ WSGI_APPLICATION = 'forecast.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+import dj_database_url
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config()
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Internationalization
