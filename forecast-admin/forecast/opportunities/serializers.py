0f4cb28f9e233f956005898e8f0ec006a8d3b3ed
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2015-09-19 10:29:15 -0400

    migrated to new folder

--- forecast-admin/forecast/opportunities/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Award
-
-class AwardSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Award
-        exclude = ('owner',)
